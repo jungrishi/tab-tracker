83e4de2907b269124edd3471e12ec6011388d422
rishiJung <you@example.com>
2019-02-10 18:59:52 +0545

    password commit updated with jwt token to register endpoint added

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -17,7 +17,11 @@ module.exports = {
   async register (req, res) {
     try{
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+          })
     } catch (err){
         res.status(400).send({
           error: 'this is used.'
